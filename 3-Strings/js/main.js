3b3ce9585fe835b2c00d05f9546206885afbe7ad
shivam-chavan-05 <shivam.chavan@gwu.edu>
2025-05-29 19:40:17 -0400

    String Lesson

---
+++ 3-Strings/js/main.js
@@ -0,0 +1,12 @@
+// Strings
+
+const str = "JavaScript";
+console.log(str.length);
+console.log(str.charAt(1));
+console.log(str.indexOf("S"));
+console.log(str.lastIndexOf("a"));
+console.log(str.slice(4, ));
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.includes("Script"));
+console.log("I am learning JavaScript".split(" "));
